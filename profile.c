cabf5508a0772b9f25a2b7d416f7602a82d34926
Ulrich Hecht <ulrich.hecht@gmail.com>
2012-11-07 11:32:51 +0100

    profile.c: make the hash function less stupid

--- profile.c
+++ profile.c
@@ -16,13 +16,13 @@ static inline int hash(void *this_fn, void *caller)
 {
     uint32_t t = (uint32_t)this_fn;
     uint32_t c = (uint32_t)caller;
-    return (t >> 16) | (t & 0xffff) | (c >> 16) | (c & 0xffff);
+    return (t >> 16) ^ (t & 0xffff) ^ (c >> 16) ^ (c & 0xffff);
 }
 
 void __cyg_profile_func_enter(void *this_fn, void *caller)
 {
-#if 1
     struct funcall *f = &calls[hash(this_fn, caller)];
+    /* XXX: hash collisions ignored */
     if (f->this_fn != this_fn) {
         f->this_fn = this_fn;
         f->caller = caller;
@@ -36,10 +36,8 @@ void __cyg_profile_func_enter(void *this_fn, void *caller)
 
 void __cyg_profile_func_exit(void *this_fn, void *caller)
 {
-#if 1
     struct funcall *f = &calls[hash(this_fn, caller)];
     f->total += libgame_utime() - f->start;
-#endif
 }
 
 void init_profile(void)
